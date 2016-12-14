3160eb7545d4f7c33365dd587dd8bb6ca3933392
张景贤 <osenyursa@gmail.com>
2016-12-14 18:46:19 +0800

    Update _columns.php

--- src/generators/default/views/_columns.php
+++ src/generators/default/views/_columns.php
@@ -57,14 +57,23 @@ return [
         'urlCreator' => function($action, $model, $key, $index) { 
                 return Url::to([$action,'<?=substr($actionParams,1)?>'=>$key]);
         },
-        'viewOptions'=>['role'=>'modal-remote','title'=>'View','data-toggle'=>'tooltip'],
-        'updateOptions'=>['role'=>'modal-remote','title'=>'Update', 'data-toggle'=>'tooltip'],
-        'deleteOptions'=>['role'=>'modal-remote','title'=>'Delete', 
-                          'data-confirm'=>false, 'data-method'=>false,// for overide yii data api
-                          'data-request-method'=>'post',
-                          'data-toggle'=>'tooltip',
-                          'data-confirm-title'=>'Are you sure?',
-                          'data-confirm-message'=>'Are you sure want to delete this item'], 
+        'viewOptions'=>[
+            'role'=>'modal-remote',
+            'title'=>'<?= $generator->generateString('View']) ?>',
+            'data-toggle'=>'tooltip'],
+        'updateOptions'=>[
+            'role'=>'modal-remote',
+            'title'=>'<?= $generator->generateString('Update']) ?>',
+            'data-toggle'=>'tooltip'],
+        'deleteOptions'=>[
+            'role'=>'modal-remote',
+            'title'=>'<?= $generator->generateString('Delete']) ?>', 
+            'data-confirm'=>false, 'data-method'=>false,// for overide yii data api
+            'data-request-method'=>'post',
+            'data-toggle'=>'tooltip',
+            'data-confirm-title'=>'<?= $generator->generateString('Are you sure?') ?>',
+            'data-confirm-message'=> '<?= $generator->generateString('Are you sure want to delete this item?') ?>'
+              ], 
     ],
 
 ];   
