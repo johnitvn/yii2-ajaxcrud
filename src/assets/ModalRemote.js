f12b473fa9b05fa122103bfc1db583e6ba90bfee
Francisco Ballina <zballinita@gmail.com>
2017-06-04 19:30:42 -0500

    Custom modal dialog no selection items

--- src/assets/ModalRemote.js
+++ src/assets/ModalRemote.js
@@ -347,6 +347,31 @@ function ModalRemote(modalId) {
     }
 
     /**
+     * Show the dialog no selection
+     * @param {string} title The title of modal
+     * @param {string} message The message for ask user
+     * @param {string} okLabel The label of ok button
+     */
+    this.noSelectionModal = function (title, message, okLabel) {
+        this.show();
+
+        if (title !== undefined) {
+            this.setTitle(title);
+        }
+        // Add form for user input if required
+        this.setContent(message);
+
+        this.addFooterButton(
+            okLabel === undefined ? this.defaults.cancelLabel : okLabel,
+            'button',
+            'btn btn-default btn-flat pull-left',
+            function (e) {
+                this.hide();
+            }
+        );
+    }
+
+    /**
      * Open the modal
      * HTML data attributes for use in local confirm
      *   - href/data-url         (If href not set will get data-url)
@@ -379,8 +404,8 @@ function ModalRemote(modalId) {
                 $(elm).hasAttr('href') ? $(elm).attr('href') : $(elm).attr('data-url'),
                 $(elm).hasAttr('data-request-method') ? $(elm).attr('data-request-method') : 'GET',
                 bulkData
-            )
-        } else {
+            );
+        }else {
             this.doRemote(
                 $(elm).hasAttr('href') ? $(elm).attr('href') : $(elm).attr('data-url'),
                 $(elm).hasAttr('data-request-method') ? $(elm).attr('data-request-method') : 'GET',
